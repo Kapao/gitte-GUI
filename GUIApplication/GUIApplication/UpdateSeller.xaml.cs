0c01a3394f6f0aa0c9771f2e275ecc7efd16b62c
Serec <soren.kaae@gmail.com>
2015-12-09 09:40:38 +0100

    removed property from updateseller

--- GUIApplication/GUIApplication/UpdateSeller.xaml.cs
+++ GUIApplication/GUIApplication/UpdateSeller.xaml.cs
@@ -25,13 +25,11 @@ namespace GUIApplication
 
         //static IPropertyService iProp = new PropertyServiceClient();
         private Seller seller;
-        private Property property;
         private List<Property> properties;
         public UpdateSeller(Seller s)
         {
             InitializeComponent();
 
-            property = new Property();
             seller = s;
             properties = seller.Properties;
             AddText();
@@ -39,8 +37,6 @@ namespace GUIApplication
 
         private void AddText()
         {
-            Property prop = properties.FirstOrDefault();
-            property.Id = prop.Id;
             txtName.Text = seller.Name;
             txtAddress.Text = seller.Address;
             txtZipCode.Text = seller.ZipCode;
@@ -50,15 +46,6 @@ namespace GUIApplication
             txtEmail.Text = seller.Email;
             txtMisc.Text = seller.Misc;
 
-            txtAddressProperty.Text = prop.Address;
-            txtZipCodeProperty.Text = prop.ZipCode;
-            txtRooms.Text = prop.Rooms.ToString();
-            txtFloors.Text = prop.Floors.ToString();
-            txtHouseSize.Text = prop.HouseSize.ToString();
-            txtLotSize.Text = prop.PropertySize.ToString();
-            txtPrice.Text = prop.Price.ToString();
-            txtConstructionYear.Text = prop.ConstructionYear.ToString();
-            txtType.Text = prop.Type;
 
         }
 
@@ -69,17 +56,6 @@ namespace GUIApplication
 
         private void BtnUpdate(object sender, RoutedEventArgs e)
         {
-            property.Address = txtAddressProperty.Text;
-            property.ZipCode = txtZipCodeProperty.Text;
-            property.Type = txtType.Text;
-            property.Rooms = Convert.ToInt32(txtRooms.Text);
-            property.Floors = Convert.ToInt32(txtFloors.Text);
-            property.HouseSize = Convert.ToDouble(txtHouseSize.Text);
-            property.PropertySize = Convert.ToDouble(txtLotSize.Text);
-            property.Price = Convert.ToDouble(txtPrice.Text);
-            property.ConstructionYear = Convert.ToInt32(txtConstructionYear.Text);
-
-
             string name = txtName.Text;
             string address = txtAddress.Text;
             string zipCode = txtZipCode.Text;
@@ -87,15 +63,13 @@ namespace GUIApplication
             string mobile = txtMobile.Text;
             string email = txtEmail.Text;
             string misc = txtMisc.Text;
-            properties.Clear();
-            properties.Add(property);
             try
             {
                 iServ.UpdateSeller(seller, properties, name, address, zipCode, phone, mobile, email, misc);
             }
             catch(Exception ex)
             {
-                MessageBox.Show("Fejl");
+                MessageBox.Show("Fejl " + ex);
             }
             this.Close();
         }
@@ -111,25 +85,6 @@ namespace GUIApplication
                 try
                 {
                     lblCity.Content = iServ.GetLocation(txtZipCode.Text).City;
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("Ugyldigt postnummer!");
-                }
-            }
-        }
-
-        private void txtZipCodeProperty_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (txtZipCodeProperty.Text == "")
-            {
-                txtZipCodeProperty.Text = "Postnummer";
-            }
-            else
-            {
-                try
-                {
-                    lblCityProperty.Content = iServ.GetLocation(txtZipCodeProperty.Text).City;
                 }
                 catch (Exception)
                 {
